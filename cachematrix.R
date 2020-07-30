ab91412835131cf446be7f5eedd310c6b7be9562
RCPellegrini <ricardo_pellegrini@outlook.com>
2020-07-31 00:29:34 +0200

    changed the test from the first line

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that are used to create a special object that stores a 
+## matrix and caches the inverse.
 
 ## Write a short comment describing this function
 
