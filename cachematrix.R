d94531dc8ce293da8e5169c8725b3e59e29e5ef7
RCPellegrini <ricardo_pellegrini@outlook.com>
2020-07-31 00:04:28 +0200

    Firt version - to be tested

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  if(!is.square.matrix(x) == TRUE) {
+    stop("this is not a valid matrix")
+    
+  }
+    inv_matrix <- NULL
+    set <- function(y) {
+      x <<- y
+      inv_matrix <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inv_matrix <<- solve
+    getsolve <- function() inv_matrix
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- x$getsolve()
+  if(!is.null(inv_matrix)) {
+    message("getting cached data")
+    return(inv_matrix)
+  }
+  data <- x$get()
+  inv_matrix <- solve(data, ...)
+  x$setsolve(inv_matrix)
+  inv_matrix
+  
 }
+
